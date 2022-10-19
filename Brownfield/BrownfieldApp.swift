707683264ed0aab41adde34353fa03b608ef1733
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-10-19 15:54:26 +0200

    Use analytics from SDK.

--- Brownfield/BrownfieldApp.swift
+++ Brownfield/BrownfieldApp.swift
@@ -13,9 +13,20 @@ struct BrownfieldApp: App {
     
     private let serviceLocator: ServiceLocator = IOSServiceLocator(userDefaults: UserDefaults.standard)
     
+    init() {
+        AnalyticsKt.doInitAnalytics(analytics: IosAnalytics())
+        AppAnalytics().appStarted()
+    }
+    
     var body: some Scene {
         WindowGroup {
             BreedListScreen(viewModel: BreedViewModel(repository: serviceLocator.breedRepository))
         }
+    }
+}
+
+class IosAnalytics: Analytics {
+    func sendEvent(eventName: String, eventArgs: [String : Any]) {
+        print("\(eventName) - \(eventArgs)")
     }
 }
