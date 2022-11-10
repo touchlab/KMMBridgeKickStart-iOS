fbb8e9726d4c7f80b5c331351a236c45dbb95a16
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-11-10 07:50:38 +0100

    Add a comment about analytics usage.

--- Brownfield/BrownfieldApp.swift
+++ Brownfield/BrownfieldApp.swift
@@ -27,6 +27,7 @@ struct BrownfieldApp: App {
 
 class IosAnalytics: Analytics {
     func sendEvent(eventName: String, eventArgs: [String : Any]) {
+        // In a real app, you would call to your analytics backend here
         print("\(eventName) - \(eventArgs)")
     }
 }
